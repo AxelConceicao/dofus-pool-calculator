d503cefec7a944f5790620d4ba434b03a2a32067
Axel Conceicao <bladingpark@gmail.com>
2020-11-18 21:03:52 +0100

    Item added method done

--- Mage.py
+++ Mage.py
@@ -1,13 +1,13 @@
 import sys
+import colorama
 from Misc import * # pylint: disable=unused-wildcard-import
-
-OBJECTS_FILENAME = './json/objects.json'
-ACTIONIDS_FILENAME = './json/actionIds.json'
+from Objects import Equipment, Rune
 
 class Mage:
     messages = {}
-    objects = None
-    actionIds = None
+    files = {}
+    rune = None
+    equipment = None
 
     def __init__(self, protocol, debug):
         self.protocol = protocol
@@ -15,12 +15,13 @@ class Mage:
         self.initMethods()
         self.loadFiles()
         for name, _ in self.messages.items():
-            obj = next((item for item in self.protocol['messages'] if item['name'] == name), None)
-            if obj is None:
+            msg = next((item for item in self.protocol['messages']
+            if item['name'] == name), None)
+            if msg is None:
                 eprint('Could not find message with name: "' + name + '"')
                 exit(1)
             else:
-                self.messages[name]['id'] = obj['protocolID']
+                self.messages[name]['id'] = msg['protocolID']
 
     def debugLogs(self, id, name, msg):
         print(name + ' (' + str(id) + ')')
@@ -28,52 +29,84 @@ class Mage:
 
     def handle(self, id, msg):
         if not id in [d['id'] for d in self.messages.values()] : return
-        name = list(self.messages.keys())[list([d['id'] for d in self.messages.values()]).index(id)]
+        name = list(self.messages.keys())[list([d['id']
+        for d in self.messages.values()]).index(id)]
         if self.debug : self.debugLogs(id, name, msg)
         self.messages[name]['method'](msg)
-        print('--')
 
     def itemAdded(self, msg):
-        print(self.objects[str(msg['object']['objectGID'])] + ':')
+        print(Style.BRIGHT + Fore.YELLOW + '[ADD] ' + Style.RESET_ALL, end='')
+        try:
+            name = self.files['objects'][str(msg['object']['objectGID'])]
+        except:
+            name = 'Unknown object'
+        effects = []
         for effect in msg['object']['effects']:
-            print(effect['actionId'])
-            obj = next((item for item in self.actionIds.values() if item['id'] == str(effect['actionId'])), None)
-            if obj is None:
-                wprint('Could not find actionId with id: "' + str(effect['actionId']) + '"')
-            else:
-                print(obj['fr'])
+            tmp = next((item for item in self.files['runes']['runes']
+            if item['boost']['id'] == effect['actionId']
+            or item['deboost']['id'] == effect['actionId']), None)
+            if tmp:
+                effects.append({
+                    'id': effect['actionId'],
+                    'value': effect['value'],
+                    'name': tmp['name'],
+                    'pool': tmp['pool']
+                })
+        obj = {
+            'name': name,
+            'UID': msg['object']['objectUID'],
+            'effects': effects
+        }
+        if len(effects) > 1:
+            self.equipment = Equipment(self.files, obj)
+        else:
+            self.rune = Rune(self.files, obj)
+        print()
 
     def itemRemoved(self, msg):
-        print('itemRemoved')
+        print(Style.BRIGHT + Fore.RED + '[REMOVE] ' + Style.RESET_ALL, end='')
+        objectUID = msg['objectUID']
+        if self.rune and self.rune.UID == objectUID:
+            print(self.rune.name, end='')
+            self.rune = None
+        elif self.equipment and self.equipment.UID == objectUID:
+            print(self.equipment.name, end='')
+            self.equipment = None
+        print()
 
     def interfaceOpened(self, msg):
-        print('Interface opened')
+        print(Style.BRIGHT + Fore.WHITE + '[UI] Workschop opened', end='')
+        print(Style.RESET_ALL)
 
     def craftResult(self, msg):
-        print('craftResult')
+        print(Style.BRIGHT + Fore.CYAN + '[FM] ' + Style.RESET_ALL, end='')
+        print(Style.BRIGHT, end='')
+        print(Style.RESET_ALL)
 
     def initMethods(self):
         self.messages = {
-            'ExchangeObjectAddedMessage': {'id': None, 'method': self.itemAdded},
-            'ExchangeObjectRemovedMessage': {'id': None, 'method': self.itemRemoved},
-            'ExchangeStartOkCraftWithInformationMessage': {'id': None, 'method': self.interfaceOpened},
-            'ExchangeCraftResultMagicWithObjectDescMessage': {'id': None, 'method': self.craftResult},
+            'ExchangeObjectAddedMessage': 
+            {'id': None, 'method': self.itemAdded},
+            'ExchangeObjectRemovedMessage': 
+            {'id': None, 'method': self.itemRemoved},
+            'ExchangeStartOkCraftWithInformationMessage': 
+            {'id': None, 'method': self.interfaceOpened},
+            'ExchangeCraftResultMagicWithObjectDescMessage': 
+            {'id': None, 'method': self.craftResult},
         }
     
     def loadFiles(self):
-        if isFileExist(OBJECTS_FILENAME):
-            with open(OBJECTS_FILENAME, 'r', encoding='utf-8') as json_file:
-                self.objects = json.load(json_file)
-        if self.objects:
-            sprint('Objects loaded')
-        else:
-            eprint('Unable to load objects')
-            exit(1)
-        if isFileExist(ACTIONIDS_FILENAME):
-            with open(ACTIONIDS_FILENAME, 'r', encoding='utf-8') as json_file:
-                self.actionIds = json.load(json_file)
-        if self.actionIds:
-            sprint('ActionIds loaded')
-        else:
-            eprint('Unable to load actionIds')
-            exit(1)
+        for fp in [
+            {'name': 'Objects', 'filename': './json/objects.json'},
+            {'name': 'Runes', 'filename':'./json/runes.json'}
+        ]:
+            if isFileExist(fp['filename']):
+                with open(fp['filename'], 'r', encoding='utf-8') as json_file:
+                    tmp = json.load(json_file)
+            if tmp:
+                self.files[fp['name'].lower()] = tmp
+                sprint(fp['name'] + ' loaded')
+            else:
+                eprint('Unable to load ' + fp['filename'])
+                exit(1)
+        print()
