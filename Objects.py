b8be247f7d17cc0488fa893bba0e9a51e1ba8392
Axel Conceicao <bladingpark@gmail.com>
2020-11-18 21:03:12 +0100

    Add objectUID to the object class

--- Objects.py
+++ Objects.py
@@ -1,15 +1,31 @@
 
 
 class Object:
-    def __init__(self, name, effects):
-        print(name + ': ', end='')
-        self.name = name
-        self.effects = effects
+    def __init__(self, files, obj):
+        self.files = files
+        self.name = obj['name']
+        self.UID = obj['UID']
+        self.effects = obj['effects']
+        self.display()
+
+    def isBoost(self, id):
+        if next((item for item in self.files['runes']['runes']
+        if item['boost']['id'] == id), None):
+            return True
+
+    def display(self):
+        print(self.name + ': ', end='')
+        tmp = []
+        for effect in self.effects:
+            space = '' if effect['name'][0] == '%' else ' '
+            dash = '' if self.isBoost(effect['id']) else '- '
+            tmp.append(dash + str(effect['value']) + space + effect['name'])
+        print(', '.join(tmp), end='')
 
 class Rune(Object):
-    def __init__(self, name, effects):
-        super().__init__(name, effects)
+    def __init__(self, files, obj):
+        super().__init__(files, obj)
 
 class Equipment(Object):
-    def __init__(self, name, effects):
-        super().__init__(name, effects)
+    def __init__(self, files, obj):
+        super().__init__(files, obj)
